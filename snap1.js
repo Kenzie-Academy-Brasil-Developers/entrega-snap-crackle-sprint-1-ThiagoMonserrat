c83a8bf1b699e187ded0af2cc6f73af9d49f246a
Thiago Henrique Monserrat Rezende <thiagohenriquemonserrat@gmail.com>
2021-08-13 16:53:32 -0300

    Atualizado0

--- snap1.js
+++ snap1.js
@@ -1,11 +1,10 @@
 let c = "Snap"
 let i = "Crackle"
 let x = "SnapCrackle"
-let maxValue = 20
 let contador = 1
 let vazio = []
-function snapCrackle() {
-  for (contador = 1; contador <= maxValue;){
+function snapCrackle(num) {
+  for (contador = 1; contador <= num;){
     if (contador % 5 === 0 && contador % 2 !== 0) {
       vazio.push(x);
     } else if (contador % 5 === 0) {
