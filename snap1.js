356b18cae6243ccde82533161c3fcbc96373bb43
Thiago Henrique Monserrat Rezende <thiagohenriquemonserrat@gmail.com>
2021-08-17 16:00:00 -0300

    Fim

--- snap1.js
+++ snap1.js
@@ -1,22 +1,18 @@
-let c = "Snap"
-let i = "Crackle"
-let x = "SnapCrackle"
-let contador = 1
-let vazio = []
-function snapCrackle(num) {
-  for (contador = 1; contador <= num;){
+function snapCrackle(num){
+  let x = " "
+  for (contador = 1; contador <= num; contador++){
     if (contador % 5 === 0 && contador % 2 !== 0) {
-      vazio.push(x);
-    } else if (contador % 5 === 0) {
-      vazio.push(i);
-    } else if (contador % 5 === 0 || contador % 2 !== 1) {
-      vazio.push(contador)
+      x = x + "SnapCrackle" + ", "
+    } else if (contador % 5 === 0 && contador % 2 === 0) {
+        x = x + "Crackle" + ", "
+    } else if (contador % 2 === 0) {
+        x = x + contador + ", "
+    } else if (contador % 5 === 0 && contador % 2 === 0) {
+        x = x + "Crackle" + ", "
     } else if (contador % 2 !== 0) {
-      vazio.push(c)
-    }
-    contador++
+        x = x + "snap" + ", "
+      } 
   }
-  let resultado = vazio.toString()
-  console.log(typeof resultado)
-  return resultado
+  console.log(x)
+  return x
 }
